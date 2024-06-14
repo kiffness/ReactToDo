bd8c6f6fe293b570371c2fa66c88d746b266bda1
Kyle Taylor <ktkage80uk@gmail.com>
2024-06-14 07:59:06 +0100

    Tests

--- Api/Todo.Tests/TodoServiceTests.cs
+++ Api/Todo.Tests/TodoServiceTests.cs
@@ -1,20 +1,6 @@
-
-
 namespace Todo.Tests;
 
-[TestFixture]
-public class TodoServiceTests(ITodoService todoService)
+public class TodoServiceTests()
 {
-    private ITodoService 
-
-    [SetUp]
-    public void Setup()
-    {
-    }
-
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
+    
 }
